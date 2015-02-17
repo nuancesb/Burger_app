34039095ba29581b13e89b1a26f665d474b23ff8
sophie <sophie.allaoua@hotmail.fr>
2015-02-17 16:02:02 +0000

    modified numbers rating to stars

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -14,4 +14,19 @@
        path: '/assets/',
        scoreName: 'rating[meat_rating]'
      });
+
+     $('#star-rating-cheese').raty({
+       path: '/assets/',
+       scoreName: 'rating[cheese_rating]'
+     });
+
+     $('#star-rating-toppings').raty({
+            path: '/assets/',
+            scoreName: 'rating[toppings_rating]'
+          });
+
+     $('#star-rating-bun').raty({
+            path: '/assets/',
+            scoreName: 'rating[bun_rating]'
+          });
    });
