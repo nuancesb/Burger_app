b7c7662dfeb62722713013bf2d782bf570a7c1de
sophie <sophie.allaoua@hotmail.fr>
2015-02-17 20:37:41 +0000

    update user model with role

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-
+  def role?(role_to_compare)
+      self.role.to_s == role_to_compare.to_s
+  end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
